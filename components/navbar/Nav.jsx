f94ac85cf994c230fb8ebadd60b7a3bd23a22e42
keplar-404 <shehabhossen76.services@gmail.com>
2023-10-03 00:43:52 +0600

    gdrive file changes

--- components/navbar/Nav.jsx
+++ components/navbar/Nav.jsx
@@ -4,36 +4,11 @@ import { useState } from "react";
 function Nav() {
   const [display, setDisplay] = useState("hidden");
 
-  // useEffect(() => {
-  //   const toBackBtn = document.querySelector("#back");
-  //   window.addEventListener("scroll", function () {
-  //     // console.log(window.scrollY);
-
-  //     if (window.scrollY > 100) {
-  //       toBackBtn.classList.remove("hidden");
-  //       toBackBtn.classList.add("block");
-  //     } else {
-  //       toBackBtn.classList.remove("block");
-  //       toBackBtn.classList.add("hidden");
-  //     }
-  //   });
-  // }, []);
-
   return (
     <>
-      {/* <div className="absolute">
-        <Image
-          src={"/backtotop.svg"}
-          width={62}
-          height={62}
-          alt="logo"
-          className="h-auto sm:w-[35px] 2xl:w-[45px] sm:right-3 md:right-7 lg:right-8 2xl:right-12  fixed bottom-6 cursor-pointer hidden"
-          id="back"
-        />
-      </div> */}
-
       <div className={`${display} absolute z-20 overflow-hidden w-full h-full`}>
         <div className=" w-screen h-screen bg-black flex flex-col justify-center items-center text-white gap-y-8 wo font-medium text-[16px]">
+          {/* for mobile */}
           <a
             className=""
             href="https://drive.google.com/file/d/1l6LHxCSBLp5QZUS-A-waMm2y-I_xbzlY/view?usp=sharing"
@@ -61,9 +36,10 @@ function Nav() {
           </div>
 
           <div className="sm:hidden lg:flex flex-row justify-center items-center wo font-medium text-[16px] gap-x-[24px]">
+            {/* for desktop */}
             <a
               target="_blank"
-              href="https://drive.google.com/file/d/1O6XG3hh-qYuhiy0tYQotyI3Lyprv97LV/view?usp=drive_link"
+              href="https://drive.google.com/file/d/1l6LHxCSBLp5QZUS-A-waMm2y-I_xbzlY/view?usp=sharing"
             >
               <button className="btn-layout btn-bg-lg  transition-all duration-500">
                 Resume
